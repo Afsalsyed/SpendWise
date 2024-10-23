a230eb400dd984544d281fb9f1f64630fc434638
afsalsyed.12 <afsalsyed.12@gmail.com>
2024-10-23 17:50:45 +0000

    Views and Serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
 from models import Category, Transaction 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('id', 'date', 'description', 'amount', 'category')
--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .serializers import CategorySerializer, TransactionSerializer
+from rest_framework.views import APIView
+from .models import Category, Transaction
 
 # Create your views here.
